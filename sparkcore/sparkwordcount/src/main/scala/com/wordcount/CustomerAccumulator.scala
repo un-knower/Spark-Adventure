6fb72ce10c99696a499cc1d9f9afb77e6cdbbf7c
Adrian <zooyoowoo@gmail.com>
2018-09-17 17:45:17 +0800

    customer

--- sparkcore/sparkwordcount/src/main/scala/com/wordcount/CustomerAccumulator.scala
+++ sparkcore/sparkwordcount/src/main/scala/com/wordcount/CustomerAccumulator.scala
@@ -31,6 +31,7 @@ class CustomerAccumulator extends AccumulatorV2[String, mutable.HashMap[String, 
 
   // 每一个分区中用于添加数据的方法 小SUM
   override def add(v: String): Unit = {
+  //_hashAcc += (v -> (_hashAcc.getOrElse(v, 0) + 1))
     _hashAcc.get(v) match {
       case None => _hashAcc += ((v, 1))
       case Some(a) => _hashAcc += ((v, a + 1))
