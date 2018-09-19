42bd123268683480d59803b100a4007ee0c09b68
Adrian <zooyoowoo@gmail.com>
2018-09-19 19:42:21 +0800

    Statistics

--- sparkcore/sparkcdn/src/main/scala/com/cdn/myCdnStatistics.scala
+++ sparkcore/sparkcdn/src/main/scala/com/cdn/myCdnStatistics.scala
@@ -6,7 +6,7 @@ import org.slf4j.LoggerFactory
 
 import scala.util.matching.Regex
 
-object CdnStatistics {
+object myCdnStatistics {
 
   val logger = LoggerFactory.getLogger(CdnStatistics.getClass)
 
@@ -31,7 +31,7 @@ object CdnStatistics {
     //ClientIP Hit/Miss ResponseTime [RequestTime] Method URL Protocol StatusCode TrafficSize Referer UserAgent
     //111.19.97.15 HIT 18 [15/Feb/2017:00:00:39 +0800] "GET http://cdn.v.abc.com.cn/videojs/video-js.css HTTP/1.1" 200 14727 "http://www.zzqbsm.com/" "Mozilla/5.0+(Linux;+Android+5.1;+vivo+X6Plus+D+Build/LMY47I)+AppleWebKit/537.36+(KHTML,+like+Gecko)+Version/4.0+Chrome/35.0.1916.138+Mobile+Safari/537.36+T7/7.4+baiduboxapp/8.2.5+(Baidu;+P1+5.1)"
 
-    //统计独立IP访问量前10位
+    //计算独立IP数   统计独立IP访问量前10位
 //    ipStatistics(input)
 
     //统计每个视频独立IP数
@@ -43,67 +43,57 @@ object CdnStatistics {
     sc.stop()
   }
 
-  // 统计独立IP访问量前10位
+  //count independent IP   | Output the number of IP access before the top 10
   def ipStatistics(data: RDD[String]): Unit = {
-    //1.统计独立IP数
-    val ipNums = data.map(x => (IPPattern.findFirstIn(x).get, 1)).reduceByKey(_ + _).sortBy(_._2, false)
-
-    //输出IP访问数前量前10位
-    ipNums.take(10).foreach(println)
-
-    println("独立IP数：" + ipNums.count())
+    data.map(x => (IPPattern.findFirstIn(x).get, 1)).reduceByKey(_ + _).sortBy(_._2, false)
+    //RegularExpression
+    val independentIP = data
+      .map(str => (str.substring(0, str.indexOf(" ")), 1))
+      .reduceByKey(_ + _)
+      .sortBy(_._2, false)
+    //Output the number of IP access before the top 10
+    independentIP.take(10).foreach(println)
+    println("The number of independent IP : " + independentIP.count)
   }
 
   // 统计每个视频独立IP数
   def videoIpStatistics(data: RDD[String]): Unit = {
-    def getFileNameAndIp(line: String) = {
-      (videoPattern.findFirstIn(line).mkString, IPPattern.findFirstIn(line).mkString)
-    }
-    //2.统计每个视频独立IP数
-    data.filter(x => x.matches(".*([0-9]+)\\.mp4.*"))
-      .map(x => getFileNameAndIp(x))
+    //记得先过滤
+    val numVideoIP = data
+      .filter(x => x.matches(".*([0-9]+)\\.mp4.*"))
+      .map(x => (videoPattern.findFirstIn(x).get, IPPattern.findFirstIn(x).get))
+      .distinct
+      .map(x => (x._1, 1))
+      .reduceByKey(_ + _)
+      .sortBy(_._2, false)
+    numVideoIP.take(10).foreach(x => println("video: " + x._1 + "\tIPnum: " + x._2))
+
+    val numVideoIP2 = data
+      .filter(x => x.matches(".*([0-9]+)\\.mp4.*"))
+      .map(x => (videoPattern.findFirstIn(x).get, IPPattern.findFirstIn(x).get))
       .groupByKey()
       .map(x => (x._1, x._2.toList.distinct))
       .sortBy(_._2.size, false)
-      .take(10)
-      .foreach(x => println("视频：" + x._1 + " 独立IP数:" + x._2.size))
+
+    numVideoIP2.take(10).foreach(x => println("video: " + x._1 + " \tIPnum: " + x._2.size))
+    println("The number of video : " + numVideoIP2.count)
   }
+
+//  100.79.121.48 HIT 33 [15/Feb/2017:00:00:46 +0800] "GET http://cdn.v.abc.com.cn/videojs/video.js HTTP/1.1" 200 174055 "http://www.abc.com.cn/" "Mozilla/4.0+(compatible;+MSIE+6.0;+Windows+NT+5.1;+Trident/4.0;)"
 
   //统计一天中每个小时间的流量
   def flowOfHour(data: RDD[String]): Unit = {
-
-    def isMatch(pattern: Regex, str: String) = {
-      str match {
-        case pattern(_*) => true
-        case _ => false
-      }
-    }
-
-    /**
-      * 获取日志中小时和http 请求体大小
-      *
-      * @param line
-      * @return
-      */
-    def getTimeAndSize(line: String) = {
-      var res = ("", 0L)
-      try {
-        val httpSizePattern(code, size) = line
-        val timePattern(year, hour) = line
-        res = (hour, size.toLong)
-      } catch {
-        case ex: Exception => ex.printStackTrace()
-      }
-      res
-    }
-
-    //3.统计一天中每个小时间的流量
-    data.filter(x=>isMatch(httpSizePattern,x))
-      .filter(x=>isMatch(timePattern,x))
-      .map(x=>getTimeAndSize(x))
-      .groupByKey()
-      .map(x=>(x._1,x._2.sum))
+    val sortedRDD = data
+      .filter(x => x.matches(".*\\s(200|206|304)\\s([0-9]+)\\s.*"))
+      .filter(x => x.matches(".*(2017):([0-9]{2}):[0-9]{2}:[0-9]{2}.*"))
+      .map(x => (x.substring(x.indexOf("[") + 13, x.indexOf("[") + 15).toInt, "(200|206|304)\\s([0-9]+)".r.findFirstIn(x).get.substring(4).toLong))
+      .reduceByKey(_ + _)
       .sortByKey()
-      .take(24)/**要take才能按顺序排序*/.foreach(x=>println(x._1+"时 CDN流量="+x._2/(1024*1024*1024)+"G"))
+
+//    println(sortedRDD.collect().mkString("\n"))
+//    println(sortedRDD.mapPartitionsWithIndex((pNum, items) => Iterator(pNum + ": [" + items.mkString(",") + "]")).collect.mkString(" "))
+
+    sortedRDD.take(24)/**要take才能按顺序排序*/.foreach(t => println(t._1 + " o'clock " + t._2.toLong / 1073741824 + "G"))
+
   }
 }
